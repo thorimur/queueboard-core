6b752c90dbac4bc6b0ebbec8097660c416083c2b
Johan Commelin <johan@commelin.net>
2024-07-22 11:10:30 +0200

    links open in new tab by default

--- dashboard.py
+++ dashboard.py
@@ -35,6 +35,7 @@ def print_html5_header():
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
     <link rel='stylesheet' href='style.css'>
+    <base target="_blank">
     </head>
     <body>
     <h1>Mathlib Review Dashboard</h1>
