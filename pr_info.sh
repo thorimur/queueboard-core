7d454c78c5dcc89267156528840f7656a08ef45a
Johan Commelin <johan@commelin.net>
2024-08-31 18:32:07 +0200

    first attempt at query to collect PR info

---
+++ pr_info.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Surface errors in this script to CI, so they get noticed.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -e -u -o pipefail
+
+OWNER=leanprover-community
+REPO=mathlib4
+
+PR_NUMBER=$1
+
+gh api graphql -f owner=$OWNER -f repo=$REPO -F prNumber=$PR_NUMBER -F query=@pr_query.graphql
