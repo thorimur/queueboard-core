749798a3d7adc890c833798ac2434efc3cd821aa
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-09-06 22:09:21 +0200

    feat: link to the github repository from the dashboard

--- dashboard.py
+++ dashboard.py
@@ -263,7 +263,8 @@ def print_html5_header() -> None:
     <h1>Mathlib review and triage dashboard</h1>""")
     # FUTURE: can this time be displayed in the local time zone of the user viewing this page?
     updated = datetime.now(timezone.utc).strftime("%B %d, %Y at %H:%M UTC")
-    print(f"<small>This dashboard was last updated on: {updated}</small>")
+    print(f"""<small>This dashboard was last updated on: {updated}<br>
+        Feedback on this dashboard is welcome, for instance <a href="https://github.com/jcommelin/queueboard">directly on the github repository</a>.</small>""")
 
 def print_html5_footer() -> None:
     print("""
