6545c356740e3a8d4d7ac83a016e0d681b127da5
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-09-24 21:24:02 +0200

    Fix stale comments; missed in the previous commits.

--- dashboard.sh
+++ dashboard.sh
@@ -42,14 +42,13 @@ query(\$endCursor: String) {
 # Query Github API for all pull requests that are on the #queue.
 # So we want to list all pull requests that are
 # - open, not draft
-# - has status:success (which excluded failing or in-progress CI)
-# - do not have any of the following labels: blocked-by-other-PR, merge-conflict, awaiting-CI, WIP, awaiting-author, delegated, auto-merge-after-CI, ready-to-merge
+# - has status:success (which excludes failing or in-progress CI)
+# - do not have any of the following labels: blocked-by-other-PR, merge-conflict, awaiting-CI, WIP, awaiting-author, awaiting-zulip, help-wanted, please-adopt delegated, auto-merge-after-CI, ready-to-merge
 queue_labels_but_merge="-label:blocked-by-other-PR -label:awaiting-CI -label:awaiting-author -label:awaiting-zulip -label:please-adopt -label:help-wanted -label:WIP -label:delegated -label:auto-merge-after-CI -label:ready-to-merge"
 QUERY_QUEUE=$(prepare_query "sort:updated-asc is:pr state:open -is:draft status:success base:master $queue_labels_but_merge -label:merge-conflict")
 gh api graphql --paginate --slurp -f query="$QUERY_QUEUE" | jq '{"output": .}' > queue.json
 
-# Query Github API for all pull requests with a merge conflict, that would be otherwise ready for review,
-# excluding those which are awaiting zulip or labelled please-adopt or help-wanted.
+# Query Github API for all pull requests with a merge conflict, that would be otherwise ready for review.
 QUERY_QUEUE_BUT_MERGE_CONFLICT=$(prepare_query "sort:updated-asc is:pr state:open -is:draft status:success base:master $queue_labels_but_merge label:merge-conflict")
 gh api graphql --paginate --slurp -f query="$QUERY_QUEUE_BUT_MERGE_CONFLICT" | jq '{"output": .}' > needs-merge.json
 
