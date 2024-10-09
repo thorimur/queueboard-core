2ccbc324f6720590ba2ffd075bbc56bb4321f107
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-09 23:17:37 +0200

    chore: add a referrer policy to the generated page
    This is another best practice, which is easy to enforce.

--- dashboard.py
+++ dashboard.py
@@ -375,6 +375,7 @@ HTML_HEADER = '''
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="referrer" content="no-referrer">
 <title>Mathlib review and triage dashboard</title>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
     integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
