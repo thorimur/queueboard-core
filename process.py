cecb9997a3b7068159ceaa3b9fe25a9d40d59a58
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-18 17:50:41 +0200

    refactor(process.py): extract parsing a JSON file with errors into a separate function

--- process.py
+++ process.py
@@ -16,6 +16,22 @@ from datetime import datetime, timezone
 from typing import List
 
 
+# Parse the JSON file 'name' for PR 'number'. Returned the parsed file if successful,
+# and an error message describing what went wrong otherwise.
+def parse_json_file(name: str, pr_number: str) -> dict | str:
+    data = None
+    with open(name, "r") as fi:
+        try:
+            data = json.load(fi)
+        except json.decoder.JSONDecodeError:
+            return f"error: the pr_info file for PR {pr_number} is invalid JSON, ignoring"
+    if "errors" in data:
+        return f"warning: the data for PR {pr_number} is incomplete, ignoring"
+    elif "data" not in data:
+        return f"warning: the data for PR {pr_number} is incomplete (perhaps a time out downloading it), ignoring"
+    return data
+
+
 def main():
     output = dict()
     updated = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -31,71 +47,54 @@ def main():
     # Read all pr info files in the data directory.
     pr_names: List[str] = sorted(os.listdir("data"))
     for pr_number in pr_names:
-        with open(f"data/{pr_number}/pr_info.json", "r") as fi:
-            data = None
-            # Handle data with errors gracefully: warn, but do not fail the script.
-            has_error = False
-            data = None
-            try:
-                data = json.load(fi)
-            except json.decoder.JSONDecodeError:
+        match parse_json_file(f"data/{pr_number}/pr_info.json", pr_number):
+            case str(err):
                 if pr_number not in known_erronerous:
-                    print("attention: found an unexpected error!")
-                    print(f"error: the pr_info file for PR {pr_number} is invalid JSON, ignoring", file=sys.stderr)
-                continue
-            if "errors" in data:
-                if pr_number not in known_erronerous:
-                    print("attention: found an unexpected error!")
-                    print(f"warning: the data for PR {pr_number} is incomplete, ignoring", file=sys.stderr)
-                continue
-            elif "data" not in data:
-                if pr_number not in known_erronerous:
-                    print("attention: found an unexpected error!")
-                    print(f"warning: the data for PR {pr_number} is incomplete (perhaps a time out downloading it), ignoring", file=sys.stderr)
-                continue
-            if pr_number in known_erronerous:
-                print(f"warning: PR {pr_number} had fine data, but was listed as erronerous: please remove it from that list")
-            inner = data["data"]["repository"]["pullRequest"]
-            number = inner["number"]
-            base_branch = inner["baseRefName"]
-            is_draft = inner["isDraft"]
-            state = inner["state"].lower()
-            last_updated = inner["updatedAt"]
-            # We assume the author URL is determined by the github handle: in practice, it is.
-            author = inner["author"]["login"]
-            title = inner["title"]
-            additions = inner["additions"]
-            deletions = inner["deletions"]
-            # Number of files modified by this PR.
-            files = len(inner["files"]["nodes"])
-            # Names of all labels applied to this PR: missing the background colour!
-            labels = [lab["name"] for lab in inner["labels"]["nodes"]]
-            assignees = [ass["login"] for ass in inner["assignees"]["nodes"]]
-            CI_passes = False
-            # Get information about the latest CI run. We just look at the "summary job".
-            CI_runs = inner["statusCheckRollup"]["contexts"]["nodes"]
-            for r in CI_runs:
-                # Ignore bors runs: these don't have a job name (and are not interesting for us).
-                if "context" in r:
-                    pass
-                elif r["name"] == "Summary":
-                    CI_passes = True if r["conclusion"] == "SUCCESS" else False
-            d = {
-                "number": number,
-                "is_draft": is_draft,
-                "CI_passes": CI_passes,
-                "base_branch": base_branch,
-                "state": state,
-                "last_updated": last_updated,
-                "author": author,
-                "title": title,
-                "additions": additions,
-                "deletions": deletions,
-                "num_files": files,
-                "label_names": labels,
-                "assignees": assignees,
-            }
-            pr_data.append(d)
+                    print(f"attention: found an unexpected error!\n{err}", file=sys.stderr)
+            case dict(data):
+                if pr_number in known_erronerous:
+                    print(f"warning: PR {pr_number} had fine data, but was listed as erronerous: please remove it from that list", file=sys.stderr)
+                inner = data["data"]["repository"]["pullRequest"]
+                number = inner["number"]
+                base_branch = inner["baseRefName"]
+                is_draft = inner["isDraft"]
+                state = inner["state"].lower()
+                last_updated = inner["updatedAt"]
+                # We assume the author URL is determined by the github handle: in practice, it is.
+                author = inner["author"]["login"]
+                title = inner["title"]
+                additions = inner["additions"]
+                deletions = inner["deletions"]
+                # Number of files modified by this PR.
+                files = len(inner["files"]["nodes"])
+                # Names of all labels applied to this PR: missing the background colour!
+                labels = [lab["name"] for lab in inner["labels"]["nodes"]]
+                assignees = [ass["login"] for ass in inner["assignees"]["nodes"]]
+                CI_passes = False
+                # Get information about the latest CI run. We just look at the "summary job".
+                CI_runs = inner["statusCheckRollup"]["contexts"]["nodes"]
+                for r in CI_runs:
+                    # Ignore bors runs: these don't have a job name (and are not interesting for us).
+                    if "context" in r:
+                        pass
+                    elif r["name"] == "Summary":
+                        CI_passes = True if r["conclusion"] == "SUCCESS" else False
+                d = {
+                    "number": number,
+                    "is_draft": is_draft,
+                    "CI_passes": CI_passes,
+                    "base_branch": base_branch,
+                    "state": state,
+                    "last_updated": last_updated,
+                    "author": author,
+                    "title": title,
+                    "additions": additions,
+                    "deletions": deletions,
+                    "num_files": files,
+                    "label_names": labels,
+                    "assignees": assignees,
+                }
+                pr_data.append(d)
     output["pr_statusses"] = pr_data
     with open("processed_data/aggregate_pr_data.json", "w") as f:
         print(json.dumps(output, indent=4), file=f)
