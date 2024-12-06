93182ea3bf13e6bfec12f3451c0541df5887c98e
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-12-06 13:54:48 +0100

    chore: also remove broken data for stubborn PRs, properly

--- check_data_integrity.py
+++ check_data_integrity.py
@@ -248,6 +248,8 @@ def main() -> None:
     # Prune broken data for all PRs, and remove superfluous entries from 'missing_prs.txt'.
     for pr in normal_prs_with_errors:
         remove_broken_data(pr)
+    for pr in stubborn_prs_with_errors:
+        shutil.rmtree(os.path.join("data", f"{pr}-basic"))
     current_missing_entries = prune_missing_prs_files()
     print(f"info: found {len(normal_prs_with_errors)} PRs with broken data")
 
