2d3326371d5aa1cad57f264c2815e7833fe9516e
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-01-07 19:22:48 +0100

    chore: fix grammar

--- dashboard.py
+++ dashboard.py
@@ -281,7 +281,7 @@ def read_json_files() -> JSONInputData:
         with open(sys.argv[i]) as prfile:
             open_prs = _extract_prs(json.load(prfile))
             if len(open_prs) >= 900:
-                print(f"warning: file {sys.argv[i]} contains at least 900 PRs: the REST API will never return more than 1000 PR. Please split the list into more files as necessary.")
+                print(f"warning: file {sys.argv[i]} contains at least 900 PRs: the REST API will never return more than 1000 PRs. Please split the list into more files as necessary.")
             all_open_prs.extend(open_prs)
     with open(path.join("processed_data", "open_pr_data.json"), "r") as f:
         aggregate_info = parse_aggregate_file(json.load(f))
