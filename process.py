416c354a00340569d049f315a2b2e72df98403f2
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-01-23 13:49:39 +0100

    chore(process.py): do not warn about PRs with exactly 100 events any more

--- process.py
+++ process.py
@@ -148,16 +148,10 @@ def get_aggregate_data(pr_data: dict, only_basic_info: bool) -> dict:
             17374, 17532, 18007, 18421, 18830, 19494, 19984, 20392, 20402,
         ]
         # Until cursor handling has been implemented, these warnings do not add helpful information.
-        if num_events == 250 and len(events_not_commit) == 0:
-            pass  # print(f"process.py: {state} PR {number} has exactly 250 events, all of which are commits: probably this data is incomplete!", file=sys.stderr)
-        elif num_events == 250:
-            pass  # print(f"process.py: {state} PR {number} has exactly 250 events: probably this data is incomplete, please double-check!", file=sys.stderr)
-        elif num_events == 100:
-            if number not in do_not_redownload:
-                if len(events_not_commit) == 0:
-                    print(f"process.py: {state} PR {number} has exactly 100 events, all of which are commits: probably this data is incomplete", file=sys.stderr)
-                else:
-                    print(f"process.py: {state} PR {number} has exactly 100 events\n  Please double-check completeness and re-download if needed.", file=sys.stderr)
+        # if num_events == 250 and len(events_not_commit) == 0:
+        #     print(f"process.py: {state} PR {number} has exactly 250 events, all of which are commits: probably this data is incomplete!", file=sys.stderr)
+        # elif num_events == 250:
+        #     print(f"process.py: {state} PR {number} has exactly 250 events: probably this data is incomplete, please double-check!", file=sys.stderr)
     return aggregate_data
 
 
