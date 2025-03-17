4b7e3375a40933930a3a2c762a5da3794353f822
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-17 11:31:13 +0100

    chore: synchronise backfill.sh with download_missing_outdated_prs.sh

--- backfill.sh
+++ backfill.sh
@@ -17,34 +17,38 @@ CURRENT_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 # We iterate over each argument, assume it is a PR number
 # and attempt to download data for that numbered PR.
-# We skip PRs which already have *any* data present (even erronerous JSON files, outdated files
-# or just some of the files): such advanced checks are left to other scripts.
+
+# This script does not perform any rate limiting: make sure to do so yourself!
+
+stubborn_prs=$(cat "stubborn_prs.txt" | grep --invert-match "^--" | sort | uniq)
+
+# NB: keep this function in sync with download_missing_outdated_PRs.sh
+# |download_normal $pr| downloads 'normal' info for the PR '$pr' into the appropriate directory.
+# Do not use with stubborn PRs: usually, this would time out.
+function download_normal {
+    local dir="data/$1"
+    local tmpdir="data/$1-temp"
+    mkdir -p "$tmpdir"
+    # Run pr_info.sh and pr_reactions.sh and save the output.
+    # "parse error: Invalid numeric literal at line N, column M'" comes from jq complaining about e.g. an empty file.
+
+    # Save the output to a temporary directory, which we delete in case anything goes wrong.
+    { ./pr_info.sh "$1" | jq '.' > "$tmpdir/pr_info.json"; } || { rm -r -f $tmpdir && return 1; }
+    # Save the current timestamp.
+    echo "$CURRENT_TIME" > "$tmpdir/timestamp.txt"
+    { ./pr_reactions.sh "$1" | jq '.' > "$tmpdir/pr_reactions.json"; } || { rm -r -f $tmpdir && return 1; }
+    rm -r -f $dir
+    mv -f $tmpdir/ $dir/
+}
 
 for prnumber in "$@"
 do
-  # Check if the directory exists
-  if [ -d "data/$prnumber" ]; then
-    echo "[skip] Data exists for PR #$prnumber: $CURRENT_TIME"
-    continue
+  if [[ $stubborn_prs == *$prnumber* ]]; then
+    echo "PR $prnumber is stubborn: backfilling these is not supported yet!"
+  else
+    echo "Backfilling/re-downloading data for PR #$prnumber"
+    download_normal $prnumber
   fi
-  CURRENT_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-  echo "Backfilling PR #$prnumber: $CURRENT_TIME"
-
-  # Create the directory for the PR.
-  dir="data/$prnumber"
-  mkdir -p "$dir"
-
-  # Run pr_info.sh and save the output.
-  ./pr_info.sh "$prnumber" | jq '.' > "$dir/pr_info.json"
-
-  # Run pr_reactions.sh and save the output.
-  ./pr_reactions.sh "$prnumber" | jq '.' > "$dir/pr_reactions.json"
-
-  # Save the current timestamp.
-  echo "$CURRENT_TIME" > "$dir/timestamp.txt"
-
-  # Sleep for 2 minutes to avoid rate limiting.
-  sleep 2m
 done
 
-echo "Backfilling run completed successfully"
+echo "Backfilling/re-downloading run completed successfully"
