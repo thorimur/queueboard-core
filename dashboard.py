6fd849bcff1d43ee37baf4bb45151ec32298d1f6
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-17 23:17:01 +0100

    temp: make users list non-searchable for the moment

--- dashboard.py
+++ dashboard.py
@@ -236,7 +236,7 @@ $(document).ready( function () {
     stateDuration: 0,
     pageLength: pageLength,
     "searching": true,
-    columnDefs: [{ type: 'diff_stat', targets: 5 }, { visible: false, targets: [3, 8] }],
+    columnDefs: [{ type: 'diff_stat', targets: 5 }, { visible: false, targets: [3, 8] }, { searchable: false, targets: 8} ],
   };
   if (params.has("search")) {
     options.search = {
