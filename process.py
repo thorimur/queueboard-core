90e1561cfbe049058889467d54ddfaf23976a457
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-04-07 15:08:47 +0200

    chore: ignore PR 3200 also, its metadata is bad

--- process.py
+++ process.py
@@ -89,6 +89,7 @@ def _compute_status_change_data(pr_data: dict, number: int, is_incomplete: bool)
         12435, 12488, 12561, 13149, 13248, 13270, 13273, 13697, 14008, 14065,
         13089, # TODO: investigate more closely!
         6595, # TODO: investigate more closely!
+        3200,
     ]
     if number in bad_prs:
         missing = {"status": "missing"}
