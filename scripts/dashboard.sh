6bafe42770ba4884d5b24e427586b02ea5e4ac06
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-07-30 22:21:58 +0200

    fix(dashboard.sh): split input data in three
    The previous split has reached its limits, which led to cascade failures
    in the data integrity check.

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -35,11 +35,13 @@ query(\$endCursor: String) {
 }
 
 # Query Github API for all open pull requests:
-# split in two as the REST-based API only returns up to 1000 items.
-QUERY_ALLOPEN1=$(prepare_query 'sort:updated-asc is:pr state:open -label:merge-conflict')
-QUERY_ALLOPEN2=$(prepare_query 'sort:updated-asc is:pr state:open label:merge-conflict')
+# split in three as the REST-based API only returns up to 1000 items.
+QUERY_ALLOPEN1=$(prepare_query 'sort:updated-asc is:pr state:open -label:merge-conflict -label:blocked-by-other-PR')
+QUERY_ALLOPEN2=$(prepare_query 'sort:updated-asc is:pr state:open label:merge-conflict -label:blocked-by-other-PR')
+QUERY_ALLOPEN3=$(prepare_query 'sort:updated-asc is:pr state:open label:blocked-by-other-PR')
 gh api graphql --paginate --slurp -f query="$QUERY_ALLOPEN1" | jq '{"output": .}' > all-open-PRs-1.json
 gh api graphql --paginate --slurp -f query="$QUERY_ALLOPEN2" | jq '{"output": .}' > all-open-PRs-2.json
+gh api graphql --paginate --slurp -f query="$QUERY_ALLOPEN3" | jq '{"output": .}' > all-open-PRs-3.json
 
 # TEMPORARILY download the old files for the queue and "just a merge conflict", to compare results.
 queue_labels_but_merge="-label:blocked-by-other-PR -label:blocked-by-core-PR -label:blocked-by-batt-PR -label:blocked-by-qq-PR -label:awaiting-CI -label:awaiting-author -label:awaiting-zulip -label:please-adopt -label:help-wanted -label:WIP -label:delegated -label:auto-merge-after-CI -label:ready-to-merge"
